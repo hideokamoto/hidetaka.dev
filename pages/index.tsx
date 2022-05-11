c522ef9baa9f3bb6f1b9ae7b9051c74dd9f92275
Hidetaka Okamoto <hideokamoto@stripe.com>
2022-05-11 17:07:43 +0900

    profile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
 import { Hero } from '../components/Hero/Hero'
+import { Profile } from '../components/Profile/Profile'
 
 const Home: NextPage = () => {
   return (
     <>
       <Hero />
+      <div className='mt-10'>
+        <Profile />
+      </div>
     </>
   )
 }
